49ee81a2be23f7502b255b5a25dc1e5800778233
Marcel Widmer <marzelwidmer@gmail.com>
2019-02-13 22:49:51 +0100

    acutator allowed GET

--- src/main/kotlin/ch/c3smonkey/cloud/monkeyapp/MonkeyAppApplication.kt
+++ src/main/kotlin/ch/c3smonkey/cloud/monkeyapp/MonkeyAppApplication.kt
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 
-@CrossOrigin(origins = arrayOf("*"), maxAge = 3600)
 @SpringBootApplication
 class MonkeyAppApplication
 
